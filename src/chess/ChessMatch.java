7603248b98737ab0eac3e8ca4df105614ce7ade8
Matheus Lui <matheuszago134@gmail.com>
2024-08-15 19:30:18 -0300

    Manipulating captured pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -12,6 +15,10 @@ public class ChessMatch {
 	private Board board;
 	private int turn;
 	private Color currentPlayer;
+	
+	//O certo seria ChessPiece, mas como nn faz tanta diferencia pra faciliar deixou Piece
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 	
 	public ChessMatch() {
 		board = new Board(8, 8);
@@ -65,6 +72,11 @@ public class ChessMatch {
 		Piece p = board.removePiece(source); //Vai remover a peça na posição de origem
 		Piece capturedPiece	 = board.removePiece(target); //Se tiver uma peça na posição de destino ela tbm será removida
 		board.placePiece(p, target);
+		
+		if(capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 	
@@ -97,6 +109,7 @@ public class ChessMatch {
 	//Esse método coloca a peça e 'traduz' a posição do xadrez para a posição da matirz
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition()) ;
+		piecesOnTheBoard.add(piece);
 	}
 	
 	private void initialSetup() {
