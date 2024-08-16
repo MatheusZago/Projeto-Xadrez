8341b4bd5553d5808d62c67f75b10a348b6f5e76
Matheus Lui <matheuszago134@gmail.com>
2024-08-15 21:55:14 -0300

    Fixed check

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,16 +56,18 @@ public class UI {
 			printBoard(chessMatch.getPieces());
 			System.out.println();
 			printCapturedPieces(captured);
-			
-			if(!chessMatch.getCheckMate()) {				
-				System.out.println();
-				System.out.println("Turn : " + chessMatch.getTurn());
+			System.out.println();
+			System.out.println("Turn : " + chessMatch.getTurn());
+			if (!chessMatch.getCheckMate()) {
 				System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-			} else {
+				if (chessMatch.getCheck()) {
+					System.out.println("CHECK!");
+				}
+			}
+			else {
 				System.out.println("CHECKMATE!");
 				System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 			}
-			
 		}
 		
 		public static void printBoard(ChessPiece[][] pieces) {
